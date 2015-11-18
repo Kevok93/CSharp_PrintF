bd3bf069a32d49409623c03db3b0b13893b7f6e2
Kevok <Kevok93@gmail.com>
2015-11-18 17:34:27 -0500

    Finalizing glibc tests. All tests are now written, and thus bugfixing begins.

--- printf_runner/glibc_printf_test.cs
+++ printf_runner/glibc_printf_test.cs
@@ -8,154 +8,147 @@ using NUnit.Framework;
 [TestFixture]
 public class glibc_printf_test
 {
-	const int DEC = -123;
-	const int INT = 255;
-	const int UNS = (~0);
-	[Test]
-	public void TestMethod() {
-		StringWriter output = new StringWriter();
-		
-		string shortstr = "Hi, Z.";
-  		string longstr = 
+	const string shortstr = "Hi, Z.";
+  	const string longstr = 
   			"Good morning, Doctor Chandra.  This is Hal.  "+
   			"I am ready for my first lesson today.";
-		fmtchk(output,"%.4x");
-		fmtchk(output,"%04x");
-		fmtchk(output,"%4.4x");
-		fmtchk(output,"%04.4x");
-		fmtchk(output,"%4.3x");
-		fmtchk(output,"%04.3x");
-		
-		fmtst1chk(output,"%.*x");
-		fmtst1chk(output,"%0*x");
-		fmtst2chk(output,"%*.*x");
-		fmtst2chk(output,"%0*.*x");
-		output.printf("bad format:\t\"%b\"\n");
- 		output.printf("nil pointer (padded):\t\"%10p\"\n", 0);  
- 		output.printf("decimal negative:\t\"%d\"\n", -2345);
-		output.printf("octal negative:\t\"%o\"\n", -2345);
-		output.printf("hex negative:\t\"%x\"\n", -2345);
-		output.printf("long decimal number:\t\"%ld\"\n", -123456L);
-		output.printf("long octal negative:\t\"%lo\"\n", -2345L);
-		output.printf("long unsigned decimal number:\t\"%lu\"\n", -123456L);
-		output.printf("zero-padded LDN:\t\"%010ld\"\n", -123456L);
-		output.printf("left-adjusted ZLDN:\t\"%-010ld\"\n", -123456L);
-		output.printf("space-padded LDN:\t\"%10ld\"\n", -123456L);
-		output.printf("left-adjusted SLDN:\t\"%-10ld\"\n", -123456L);
-		
-		output.printf("zero-padded string:\t\"%010s\"\n", shortstr);
-		output.printf("left-adjusted Z string:\t\"%-010s\"\n", shortstr);
-		output.printf("space-padded string:\t\"%10s\"\n", shortstr);
-		output.printf("left-adjusted S string:\t\"%-10s\"\n", shortstr);
-		output.printf("null string:\t\"%s\"\n", null);
-		output.printf("limited string:\t\"%.22s\"\n", longstr);
-		
-		output.printf("e-style >= 1:\t\"%e\"\n", 12.34);
-		output.printf("e-style >= .1:\t\"%e\"\n", 0.1234);
-		output.printf("e-style < .1:\t\"%e\"\n", 0.001234);
-		output.printf("e-style big:\t\"%.60e\"\n", 1e20);
-		output.printf ("e-style == .1:\t\"%e\"\n", 0.1);
-		output.printf("f-style >= 1:\t\"%f\"\n", 12.34);
-		output.printf("f-style >= .1:\t\"%f\"\n", 0.1234);
-		output.printf("f-style < .1:\t\"%f\"\n", 0.001234);
-		output.printf("g-style >= 1:\t\"%g\"\n", 12.34);
-		output.printf("g-style >= .1:\t\"%g\"\n", 0.1234);
-		output.printf("g-style < .1:\t\"%g\"\n", 0.001234);
-		output.printf("g-style big:\t\"%.60g\"\n", 1e20);
-		
-		output.printf (" %6.5f\n", .099999999860301614);
-		output.printf (" %6.5f\n", .1);
-		output.printf ("x%5.4fx\n", .5);
-		
-		output.printf ("%#03x\n", 1);
-		{
-			double d = float.MinValue;
-			int niter = 17;
-			
-			while (niter-- != 0) output.printf ("%.17e\n", d / 2);
+  			
+	static StringWriter report = new StringWriter();
+	
+	[	TestCase("0012",					"%.4x",		0x12, 					TestName="Precision"						)]
+	[	TestCase("0012",					"%04x",		0x12, 					TestName="Zero"								)]
+	[	TestCase("0012",					"%4.4x",	0x12, 					TestName="Width"							)]
+	[	TestCase("0012",					"%04.4x",	0x12,					TestName="Zero Width"						)]
+	[	TestCase(" 012",					"%4.3x",	0x12,					TestName="Width Precision"					)]
+	[	TestCase(" 012",					"%04.3x",	0x12, 					TestName="Zero Width Precision"				)]
+	[	TestCase("0012",					"%.*x",	4,	0x12,					TestName="Arbitrary Precision"				)]
+	[	TestCase("0012",					"%0*x",	4,	0x12,					TestName="Zero Arbitrary Width"				)]
+	[	TestCase("0012",					"%*.*x",	4,	4,	0x12,			TestName="Arbitrary Precision Width"		)]
+	[	TestCase("0012",					"%0*.*x",	4,	4,	0x12,			TestName="Zero Arbitrary Precision Width"	)]
+	[	TestCase("%b", 						"%b",								TestName="Bad Format"						)]
+	[	TestCase("     (nil)", 				"%10p",		0,						TestName="Null Pointer"						)]
+	[	TestCase("-2345", 					"%d",		-2345,					TestName="decimal negative"					)]
+	[	TestCase("37777773327", 			"%o",		-2345,					TestName="octal negative"					)]
+	[	TestCase("fffff6d7", 				"%x",		-2345,					TestName="hex negative"						)]
+	[	TestCase("-123456", 				"%ld",		-123456L,				TestName="long decimal number"				)]
+	[	TestCase("37777773327", 			"%lo",		-2345L,					TestName="long octal negative"				)]
+	[	TestCase("4294843840", 				"%lu",		-123456L,				TestName="long unsigned decimal number"		)]
+	[	TestCase("-000123456", 				"%010ld",	-123456L,				TestName="zero-padded LDN"					)]
+	[	TestCase("-123456   ", 				"%-010ld",	-123456L,				TestName="left-adjusted ZLDN"				)]
+	[	TestCase("   -123456", 				"%10ld",	-123456L,				TestName="space-padded LDN"					)]
+	[	TestCase("-123456   ", 				"%-10ld",	-123456L,				TestName="left-adjusted SLDN"				)]
+	[	TestCase("    Hi, Z.", 				"%010s",	shortstr,				TestName="zero-padded string"				)]
+	[	TestCase("Hi, Z.    ", 				"%-010s",	shortstr,				TestName="left-adjusted Z string"			)]
+	[	TestCase("    Hi, Z.", 				"%10s",		shortstr,				TestName="space-padded string"				)]
+	[	TestCase("Hi, Z.    ", 				"%-10s",	shortstr,				TestName="left-adjusted S string"			)]
+	[	TestCase("(null)", 					"%s",		null,					TestName="null string"						)]
+	[	TestCase("Good morning, Doctor", 	"%.20s",	longstr,				TestName="limited string"					)]
+	[	TestCase("1.234000e+01", 			"%e",		12.34,					TestName="e-style >= 1"						)]
+	[	TestCase("1.234000e-01", 			"%e",		0.1234,					TestName="e-style >= .1"					)]
+	[	TestCase("1.234000e-03", 			"%e",		0.001234,				TestName="e-style < .1"						)]
+	[	TestCase("1.000000000000000e+20",	"%.15e",	1e20,					TestName="e-style big"						)]
+	[	TestCase("1.000000e-01",		 	"%e",		0.1,					TestName="e-style == .1"					)]
+	[	TestCase("12.340000", 				"%f",		12.34,					TestName="f-style >=  1"					)]
+	[	TestCase("0.123400",	 			"%f",		0.1234,					TestName="f-style >= .1"					)]
+	[	TestCase("0.001234", 				"%f",		0.001234,				TestName="f-style <  .1"					)]
+	[	TestCase("12.34", 					"%g",		12.34,					TestName="g-style >=  1"					)]
+	[	TestCase("0.1234", 					"%g",		0.1234,					TestName="g-style >= .1"					)]
+	[	TestCase("0.001234", 				"%g",		0.001234,				TestName="g-style <  .1"					)]
+	[	TestCase("100000000000000000000",	"%.40g",	1e20,					TestName="g-style big"						)]
+	[	TestCase(" 0.10000",				" %6.5f",	.09999999,				TestName="Rounding"							)]
+	[	TestCase(" 0.10000",				" %6.5f",	.1,						TestName="Not Rounding .1"					)]
+	[	TestCase("x0.5000x",				"x%5.4fx",	.5,						TestName="Not Rounding .5"					)]
+	[	TestCase("   4.94066e-324",			"%15.5e", 	4.940656458412465e-324, TestName="Really small number"				)]
+	[	TestCase("1.234568e+06",			"%e",		1234567.8,				TestName="G consistency 1"					)]
+	[	TestCase("1234567.800000", 			"%f",		1234567.8,				TestName="G consistency 2"					)]
+	[	TestCase("1.23457e+06",				"%g",		1234567.8,				TestName="G consistency 3"					)]
+	[	TestCase("123.456",					"%g",		123.456,				TestName="G consistency 4"					)]
+	[	TestCase("1e+06",					"%g",		1000000.0,				TestName="G consistency 5"					)]
+	[	TestCase("10",						"%g",		10.0,					TestName="G consistency 6"					)]
+	[	TestCase("0.02",					"%g",		0.02,					TestName="G consistency 7"					)]
+	public void sprintfTest(String expected, String format, params Object[] args) {
+		string actual = SPrintF.sprintf(format, args);
+  		AssertOrPrint(actual, expected);
+	}
+  	
+	[SetUpFixture]
+	public class teardown {
+		[SetUp]
+		public void clearReport() {
+			glibc_printf_test.report = new StringWriter();
 		}
-		
-		output.printf ("%15.5e\n", 4.9406564584124654e-324);
-		
+		[TearDown]
+		public void printReport() {
+			Console.WriteLine(glibc_printf_test.report);
+		}
+	}
+	
+	[Test]
+	public void entropy_test () {
+		for(int i = 0; i < 50; i++) 
+			AssertOrPrint("-1.70141173319264430e+038", SPrintF.sprintf("%.17e", float.MinValue / 2));
+	}
+	[Test]
+	public void digital_test ([Range(0,15)] int testcase) {
+		const int DEC = -123;
+		const int INT = 255;
+		const int UNS = (~0);
+		string prefix = "%";
+		if ((testcase & 8) == 0) prefix += "-";
+		if ((testcase & 4) == 0) prefix += "+";
+		if ((testcase & 2) == 0) prefix += "#";
+		if ((testcase & 1) == 0) prefix += "0";
+		string format = SPrintF.sprintf("|%s6d |%s6o |%s6x |%s6X |%s6u |", prefix, prefix, prefix, prefix, prefix);
+		string output = SPrintF.sprintf(format, DEC, INT, INT, INT, UNS);
+  		AssertOrPrint(digital_test_expected[testcase], output);
+	}
+	
+	[Test]
+	public void float_test ([Range(0,8)] int testcase) {
 		string FORMAT = "|%12.4f|%12.4e|%12.4g|\n";
-		output.printf (FORMAT, 0.0, 0.0, 0.0);
-		output.printf (FORMAT, 1.0, 1.0, 1.0);
-		output.printf (FORMAT, -1.0, -1.0, -1.0);
-		output.printf (FORMAT, 100.0, 100.0, 100.0);
-		output.printf (FORMAT, 1000.0, 1000.0, 1000.0);
-		output.printf (FORMAT, 10000.0, 10000.0, 10000.0);
-		output.printf (FORMAT, 12345.0, 12345.0, 12345.0);
-		output.printf (FORMAT, 100000.0, 100000.0, 100000.0);
-		output.printf (FORMAT, 123456.0, 123456.0, 123456.0);
-		fp_test(output);
-		output.printf ("%e should be 1.234568e+06\n", 1234567.8);
-		output.printf ("%f should be 1234567.800000\n", 1234567.8);
-		output.printf ("%g should be 1.23457e+06\n", 1234567.8);
-		output.printf ("%g should be 123.456\n", 123.456);
-		output.printf ("%g should be 1e+06\n", 1000000.0);
-		output.printf ("%g should be 10\n", 10.0);
-		output.printf ("%g should be 0.02\n", 0.02);
-		output.printf ("printf (\"%%hhu\", %u) = %hhu\n", char.MaxValue + 2, char.MaxValue + 2);
-		output.printf ("printf (\"%%hu\", %u) = %hu\n", short.MaxValue + 2, short.MaxValue + 2);
-		output.printf ("printf (\"%%hhi\", %i) = %hhi\n", char.MaxValue + 2, char.MaxValue + 2);
-		output.printf ("printf (\"%%hi\", %i) = %hi\n", short.MaxValue + 2, short.MaxValue + 2);
-		
-		output.Write ("--- Should be no further output. ---");
-		Console.Out.WriteLine(output.ToString());
-}
-	
-	public void fp_test (StringWriter output) {
-	
-		output.WriteLine("Formatted output test");
-		output.printf("prefix  6d      6o      6x      6X      6u\n");
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 2; j++) {
-				for (int k = 0; k < 2; k++) {
-					for (int l = 0; l < 2; l++) {
-						string prefix = "%";
-						string tp;
-						if (i == 0) prefix += "-";
-						if (j == 0) prefix += "+";
-						if (k == 0) prefix += "#";
-						if (l == 0) prefix += "0";
-						output.printf("%5s |", prefix);
-						tp = prefix + "6d |";
-						output.printf(tp, DEC);
-						tp = prefix + "6o |";
-						output.printf(tp, INT);
-						tp = prefix + "6x |";
-						output.printf(tp, INT);
-						tp = prefix + "6X |";
-						output.printf(tp, INT);
-						tp = prefix + "6u |";
-						output.printf(tp, UNS);
-						output.printf("\n");
-					}
-				}
-			}
-		}
-		output.printf("%10s\n",null);
-		output.printf("%-10s\n",null);
+		double value = float_test_expected[testcase].Item2;
+		string output = SPrintF.sprintf(FORMAT, value, value, value);
+  		AssertOrPrint(float_test_expected[testcase].Item1, output);
 	}
 	
-	public void fmtchk (StringWriter output, string fmt) {
-		output.Write(fmt);
-		output.printf(":\t`");
-		output.printf(fmt, 0x12);
-		output.printf("'\n");
+	public void AssertOrPrint ( string s1, string s2) {
+  		try {
+  			Assert.AreEqual(s2, s1);
+  		} catch (AssertionException e) {
+			report.printf("|%35.35s| != |%35.35s|\n", s1, s2);
+			throw;
+  		}
 	}
 	
-	public void fmtst1chk (StringWriter output, string fmt) {
-		output.Write(fmt);
-		output.printf(":\t`");
-		output.printf(fmt, 4, 0x12);
-		output.printf("'\n");
-	}
+	static readonly Tuple<string,double>[] float_test_expected = {
+		new Tuple<string,double>("|      0.0000|  0.0000e+00|           0|",0.0),
+		new Tuple<string,double>("|      1.0000|  1.0000e+00|           1|",1.0),
+		new Tuple<string,double>("|     -1.0000| -1.0000e+00|          -1|",-1.0),
+		new Tuple<string,double>("|    100.0000|  1.0000e+02|         100|",100.0),
+		new Tuple<string,double>("|   1000.0000|  1.0000e+03|        1000|",1000.0),
+		new Tuple<string,double>("|  10000.0000|  1.0000e+04|       1e+04|",10000.0),
+		new Tuple<string,double>("|  12345.0000|  1.2345e+04|   1.234e+04|",12345.0),
+		new Tuple<string,double>("| 100000.0000|  1.0000e+05|       1e+05|",100000.0),
+		new Tuple<string,double>("| 123456.0000|  1.2346e+05|   1.235e+05|",123456.0),
+	};
 	
-	public void fmtst2chk (StringWriter output, string fmt) {
-		output.Write(fmt);
-		output.printf(":\t`");
-		output.printf(fmt, 4, 4, 0x12);
-		output.printf("'\n");
-	}
+	static readonly string[] digital_test_expected = {
+		"|-123   |0377   |0xff   |0XFF   |4294967295 |",
+		"|-123   |0377   |0xff   |0XFF   |4294967295 |",
+		"|-123   |377    |ff     |FF     |4294967295 |",
+		"|-123   |377    |ff     |FF     |4294967295 |",
+		"|-123   |0377   |0xff   |0XFF   |4294967295 |",
+		"|-123   |0377   |0xff   |0XFF   |4294967295 |",
+		"|-123   |377    |ff     |FF     |4294967295 |",
+		"|-123   |377    |ff     |FF     |4294967295 |",
+		"|-00123 |000377 |0x00ff |0X00FF |4294967295 |",
+		"|  -123 |  0377 |  0xff |  0XFF |4294967295 |",
+		"|-00123 |000377 |0000ff |0000FF |4294967295 |",
+		"|  -123 |   377 |    ff |    FF |4294967295 |",
+		"|-00123 |000377 |0x00ff |0X00FF |4294967295 |",
+		"|  -123 |  0377 |  0xff |  0XFF |4294967295 |",
+		"|-00123 |000377 |0000ff |0000FF |4294967295 |",
+		"|  -123 |   377 |    ff |    FF |4294967295 |"
+	};
+	
 }
